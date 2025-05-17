4f746b16548f8f4279c409211f82285e34a59ccb
Alonso <amuc23@gmail.com>
2025-05-17 16:12:09 -0400

    prueba de seguridad

--- ferremas/settings.py
+++ ferremas/settings.py
@@ -163,14 +163,21 @@ TRANSBANK = {
 
 
 
+
+# No uses load_dotenv() en producción
+if os.path.exists('.env'):
+    from dotenv import load_dotenv
+    load_dotenv()
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = f"FERREMAS <{EMAIL_HOST_USER}>"
+
 
 
 
